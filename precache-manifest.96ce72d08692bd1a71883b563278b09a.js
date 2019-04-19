2cbe089e1f1710eb900e43fcdec852eac61da1fa
Raj7839 <rajgopal.singh03@gmail.com>
2019-04-19 17:42:27 +0530

    Updates

---
+++ precache-manifest.96ce72d08692bd1a71883b563278b09a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05b3a870750a07c85985",
+    "url": "/Raj7839/react_recipe_application.git/static/js/runtime~main.41ab5656.js"
+  },
+  {
+    "revision": "5b1c7e6fd6cc9f5f7b4a",
+    "url": "/Raj7839/react_recipe_application.git/static/js/main.cd5b78aa.chunk.js"
+  },
+  {
+    "revision": "bcc9f91baed7286fdd8e",
+    "url": "/Raj7839/react_recipe_application.git/static/js/2.120c768b.chunk.js"
+  },
+  {
+    "revision": "5b1c7e6fd6cc9f5f7b4a",
+    "url": "/Raj7839/react_recipe_application.git/static/css/main.f2aca486.chunk.css"
+  },
+  {
+    "revision": "4a84b4617a7c3c9a3e47b4d1aa6febcd",
+    "url": "/Raj7839/react_recipe_application.git/index.html"
+  }
+];
